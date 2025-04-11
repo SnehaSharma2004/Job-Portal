2a92f73a83007690ed72906e07d811b3d12c8493
TARANPREET <tanu04@gmail.com>
2025-04-11 20:36:05 +0530

    Home page is created with HeroSection,Category,LatestJob and footer section

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Navbar from "./shared/Navbar";
+import HeroSection from '@/HeroSection';
+import CategoryCarousal from './CategoryCarousel'; 
+import LatestJobs from './LatestJobs';
+import Footer from './shared/Footer';
+
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
+      <HeroSection/>
+      <CategoryCarousal/>
+      <LatestJobs/>
+      <Footer/>
     </div>
   )
 }
