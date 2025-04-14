9306dda5882f344e722c38a83bcecc94fa76c054
TARANPREET <tanu04@gmail.com>
2025-04-14 12:00:29 +0530

    FilterCard Section issue is resolved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/shared/Navbar'
 import Login from './components/auth/Login'
 import Signup from './components/auth/Signup'
 import Home from './components/Home'
-//import Jobs from './components/Jobs'
+import Jobs from './components/Jobs'
 //import Browse from './components/Browse'
 /* import Profile from './components/Profile' */
 //import JobDescription from './components/JobDescription'
@@ -29,10 +29,10 @@ const appRouter = createBrowserRouter([
     path: '/signup',
     element: <Signup />
   },
-  // {
-  //   path: "/jobs",
-  //   element: <Jobs />
-  // },
+  {
+    path: "/jobs",
+    element: <Jobs />
+  }, 
   // {
   //   path: "/description/:id",
   //   element: <JobDescription />
---
+++ frontend/src/components/job.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Button } from './ui/button'
+import { Bookmark } from 'lucide-react'
+import { Avatar, AvatarImage } from './ui/avatar'
+import { Badge } from './ui/badge'
+const job = () => {
+  return (
+    <div className='p-5 rounded-md shadow-xl bg-white border boder-gray-100'>
+        <div className='flex item-center justify-between'>
+            <p className='text-sm text-gray-500'>2 days ago</p>
+            <Button variant="outline" className="rounded-full" size="icon"><Bookmark /></Button>
+        </div>
+        <div className='flex item-center gap-2 my-2'>
+            <Button className="p-6" variant="outline" size="icon">
+            <Avatar>
+                <AvatarImage src="/https://www.shutterstock.com/image-vector/circle-line-simple-design-logo-600nw-2174926871.jpg"></AvatarImage>
+            </Avatar> {/*//still issue to be resolved */}
+            </Button>
+            <div>
+              <h1 className='font-medium text-lg'>Company Name</h1>
+              <p className='text-sm text-gray-500'>India</p>
+            </div> 
+          </div>
+
+          <div>
+            <h1 className='font-bold text-lg my-2'>Title</h1>
+            <p className='tex-5m text-gray-600'>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+          </div>
+          
+          <div className='flex items-center gap-2 mt-4'>
+                <Badge className={'text-blue-700 font-bold'} variant="ghost"> Positions</Badge>
+                <Badge className={'text-[#F83002] font-bold'} variant="ghost">Part Time</Badge>
+                <Badge className={'text-[#7209b7] font-bold'} variant="ghost">LPA</Badge>
+          </div>
+          <div className='flex items-center gap-4 mt-4'>
+                {/* <Button onClick={()=> navigate(`/description/${job?._id}`)} variant="outline">Details</Button> */}
+                <Button className="bg-[#7209b7]">Save For Later</Button>
+            </div>
+          
+  </div> 
+  )
+}
+
+export default job
