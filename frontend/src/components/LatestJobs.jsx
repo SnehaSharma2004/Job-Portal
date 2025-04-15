5e111db2c4a7edd5ae89484eba5701f7876c3df7
TARANPREET <tanu04@gmail.com>
2025-04-15 10:25:54 +0530

    made description page dynamic ,login reload issue is resolved

--- frontend/src/components/LatestJobs.jsx
+++ frontend/src/components/LatestJobs.jsx
@@ -1,11 +1,10 @@
-/* import React from 'react'
+import React from 'react'
 import LatestJobCards from './LatestJobCards';
 import { useSelector } from 'react-redux'; 
 
-const randomJobs = [1, 2, 3, 4, 5, 6, 7, 8];
-//const LatestJobs=()=>{
+//const randomJobs = [1, 2, 3, 4, 5, 6, 7, 8];
 const LatestJobs = () => {
-    const { allJobs } = useSelector((state) => state.jobs);
+    const { allJobs =[] } = useSelector((state) => state.jobs || {});
    
     return (
         <div className='max-w-7xl mx-auto my-20'>
@@ -20,23 +19,5 @@ const LatestJobs = () => {
     )
 }
 
-export default LatestJobs */
-import React from 'react'
-import LatestJobCards from './LatestJobCards'
-const randomJobs = [1, 2, 3, 4, 5, 6, 7, 8];
-const LatestJobs = () => {
-  return (
-    <div className='max-w-7xl mx-auto my-20'>
-      <h1 className='text-4xl font-bold'><span className='text-[#6A38C2]'>Latest & Top </span> Job Openings</h1>
-      {/* cars */}
-      <div className='grid grid-cols-3 gap-4 my-5'>
-      {
-        randomJobs.slice(0,6).map((item,index)=><LatestJobCards/>)
-      }
-      </div>
-      
-    </div>
-  )
-}
-
 export default LatestJobs
+
--- frontend/src/components/job.jsx
+++ frontend/src/components/job.jsx
@@ -1,63 +1,55 @@
-import React from "react";
-import { Button } from "./ui/button";
-import { Bookmark } from "lucide-react";
-import { Avatar, AvatarImage } from "./ui/avatar";
-import { Badge } from "./ui/badge";
-import { Navigate  } from "react-router-dom";
-import { useNavigate } from 'react-router-dom';
+import React from 'react'
+import { Button } from './ui/button'
+import { Bookmark } from 'lucide-react'
+import { Avatar, AvatarImage } from './ui/avatar'
+import { Badge } from './ui/badge'
+import { useNavigate } from 'react-router-dom'
 
-const job = () => {
-  const navigate= useNavigate();
-  const jobId="sjdgqwebuflwnifhqwlbyegfx"
-  return (
-    <div className="p-5 rounded-md shadow-xl bg-white border boder-gray-100">
-      <div className="flex item-center justify-between">
-        <p className="text-sm text-gray-500">2 days ago</p>
-        <Button variant="outline" className="rounded-full" size="icon">
-          <Bookmark />
-        </Button>
-      </div>
-      <div className="flex item-center gap-2 my-2">
-        <Button className="p-6" variant="outline" size="icon">
-          <Avatar>
-            <AvatarImage
-              src="https://www.shutterstock.com/image-vector/circle-line-simple-design-logo-600nw-2174926871.jpg"
-              alt="profile"
-            />
-          </Avatar>{" "}
-          {/*//still issue to be resolved */}
-        </Button>
-        <div>
-          <h1 className="font-medium text-lg">Company Name</h1>
-          <p className="text-sm text-gray-500">India</p>
+const Job = ({job}) => {
+    const navigate = useNavigate();
+    // const jobId = "lsekdhjgdsnfvsdkjf";
+
+    const daysAgoFunction = (mongodbTime) => {
+        const createdAt = new Date(mongodbTime);
+        const currentTime = new Date();
+        const timeDifference = currentTime - createdAt;
+        return Math.floor(timeDifference/(1000*24*60*60));
+    }
+    
+    return (
+        <div className='p-5 rounded-md shadow-xl bg-white border border-gray-100'>
+            <div className='flex items-center justify-between'>
+                <p className='text-sm text-gray-500'>{daysAgoFunction(job?.createdAt) === 0 ? "Today" : `${daysAgoFunction(job?.createdAt)} days ago`}</p>
+                <Button variant="outline" className="rounded-full" size="icon"><Bookmark /></Button>
+            </div>
+
+            <div className='flex items-center gap-2 my-2'>
+                <Button className="p-6" variant="outline" size="icon">
+                    <Avatar>
+                        <AvatarImage src={job?.company?.logo} />
+                    </Avatar>
+                </Button>
+                <div>
+                    <h1 className='font-medium text-lg'>{job?.company?.name}</h1>
+                    <p className='text-sm text-gray-500'>India</p>
+                </div>
+            </div>
+
+            <div>
+                <h1 className='font-bold text-lg my-2'>{job?.title}</h1>
+                <p className='text-sm text-gray-600'>{job?.description}</p>
+            </div>
+            <div className='flex items-center gap-2 mt-4'>
+                <Badge className={'text-blue-700 font-bold'} variant="ghost">{job?.position} Positions</Badge>
+                <Badge className={'text-[#F83002] font-bold'} variant="ghost">{job?.jobType}</Badge>
+                <Badge className={'text-[#7209b7] font-bold'} variant="ghost">{job?.salary}LPA</Badge>
+            </div>
+            <div className='flex items-center gap-4 mt-4'>
+                <Button onClick={()=> navigate(`/description/${job?._id}`)} variant="outline">Details</Button>
+                <Button className="bg-[#7209b7]">Save For Later</Button>
+            </div>
         </div>
-      </div>
+    )
+}
 
-      <div>
-        <h1 className="font-bold text-lg my-2">Title</h1>
-        <p className="tex-5m text-gray-600">
-          Lorem ipsum dolor sit amet, consectetur adipisicing.
-        </p>
-      </div>
-
-      <div className="flex items-center gap-2 mt-4">
-        <Badge className={"text-blue-700 font-bold"} variant="ghost">
-          {" "}
-          Positions
-        </Badge>
-        <Badge className={"text-[#F83002] font-bold"} variant="ghost">
-          Part Time
-        </Badge>
-        <Badge className={"text-[#7209b7] font-bold"} variant="ghost">
-          LPA
-        </Badge>
-      </div>
-      <div className="flex items-center gap-4 mt-4">
-        <Button onClick={()=> navigate(`/description/${jobId}`)} variant="outline">Details</Button> 
-        <Button className="bg-[#7209b7]">Save For Later</Button>
-      </div>
-    </div>
-  );
-};
-
-export default job;
+export default Job
