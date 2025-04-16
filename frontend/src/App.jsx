b5f0e1e098797cb03e23e4fd958116b4fbbfe6ec
SnehaSharma2004 <sneha.sharmax04@gmail.com>
2025-04-16 11:51:51 +0530

    admin functionality added, previous issues are resolved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,14 +7,14 @@ import Jobs from './components/Jobs'
 import Browse from './components/Browse'
 import Profile from './components/Profile'
 import JobDescription from './components/JobDescription'
-/* import Companies from './components/admin/Companies'
+import Companies from './components/admin/Companies'
 import CompanyCreate from './components/admin/CompanyCreate'
 import CompanySetup from './components/admin/CompanySetup'
 import AdminJobs from "./components/admin/AdminJobs";
 import PostJob from './components/admin/PostJob'
-import Applicants from './components/admin/Applicants'
+import Applicants from './components/admin/Applicant'
 import ProtectedRoute from './components/admin/ProtectedRoute'
- */
+
 
 const appRouter = createBrowserRouter([
   {
@@ -44,13 +44,13 @@ const appRouter = createBrowserRouter([
   {
     path: "/profile",
     element: <Profile />
-  }
+  },
   // admin ke liye yha se start hoga
-  /* {
+   {
     path:"/admin/companies",
     element: <ProtectedRoute><Companies/></ProtectedRoute>
   },
-  {
+ {
     path:"/admin/companies/create",
     element: <ProtectedRoute><CompanyCreate/></ProtectedRoute> 
   },
@@ -70,7 +70,7 @@ const appRouter = createBrowserRouter([
     path:"/admin/jobs/:id/applicants",
     element:<ProtectedRoute><Applicants/></ProtectedRoute> 
   },
- */
+
 ])
 function App() {
 
